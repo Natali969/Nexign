16c31c2703c61f146796cc2d425cace626d82189
Natali969 <81923119+Natali969@users.noreply.github.com>
2025-05-08 20:08:06 +0300

    Add files via upload

--- AutoTests/generate_cdrs.py
+++ AutoTests/generate_cdrs.py
@@ -4,140 +4,177 @@ from faker import Faker
 from psycopg2 import Error
 from modulesCDR.db_connection import db_connection_brt
 from modulesCDR.work_db import get_subscribers_list
+from modulesCDR.calls_intersect import is_calls_intersect
 
 
 fake = Faker('ru_RU')
 outgoing_call = '01'
 ingoing_call = '02'
-subscribers_list = get_subscribers_list()
-# генерация записи по абоненту
-def generate_cdr_entry():
-    # список номеров абонентов "Ромашки" из БД
-    subscribers_list = get_subscribers_list()
+subscribers_list = get_subscribers_list("SELECT msisdn FROM subscribers LiMIT 10")
+
+# список номеров абонентов "Ромашки" из БД
+request = "SELECT msisdn FROM subscribers LiMIT 10"
+subscribers_list = get_subscribers_list(request)
+
+# список номеров абонентов "Ромашки" из БД с тарифом Классика
+request = "SELECT msisdn FROM subscribers WHERE tariff_id = 1 LiMIT 10"
+subscribers_classic_list = get_subscribers_list(request)
+
+# список номеров абонентов "Ромашки" из БД с тарифом Помесячный
+request = "SELECT msisdn FROM subscribers WHERE tariff_id = 2 LiMIT 10"
+subscribers_month_list = get_subscribers_list(request)
+
+# генерирует запись, исходящую от нашего абонента абоненту другого оператора
+def generate_cdr_from_our_to_other(subscribers_list):
     # тип вызова: исходящий или входящий
     call_type = random.choice([outgoing_call, ingoing_call])
 
     # номер абонента должен выбираться из БД запросом SELECT,
     # а дальше - рандомный выбор в коде
 
-    # временная замена - список номеров абонентов
     # обслуживаемый абонент выбирается из БД
     our_subscriber = random.choice(subscribers_list)
-    subscribers_list.remove(our_subscriber)
-    # с кем осуществлялась связь - генерируется случайно
-    other_subscriber = f'7{random.randint(900, 999)}{"".join(random.choices("0123456789", k=7))}'
 
-    # если разговор был между двумя абонентами оператора "ромашка"
-    double_call = True if other_subscriber in subscribers_list else False
+    other_subscriber = f'7921{"".join(random.choices("0123456789", k=7))}'
 
     # генерируется дата и время начала звонка
-    start_datetime = fake.date_time_between(start_date='-1y', end_date='-1d')
+    # date_part = fake.date_time_between(start_date=start_date, end_date=end_date).date()
+    date_part = fake.date_between(start_date='-1y', end_date='-1d')
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+    
+    start_datetime = datetime.datetime(date_part.year, date_part.month, date_part.day, hour, minute, second)
+
     # продолжительность звонка от 1 секунды до получаса
-    duration = random.randint(1, 72000)
+    duration = random.randint(1, 1800)
 
     # дата и время окончания звонка
     end_datetime = start_datetime + datetime.timedelta(seconds=duration)
 
-    # если разговор пересёк границу полуночи
-    midnight_call = True if start_datetime.date() != end_datetime.date() else False
+    # генерируем запись
+    entries = []
+    entries.append([call_type, our_subscriber, other_subscriber,
+                            start_datetime.isoformat(), end_datetime.isoformat()])
+    return entries
+
+# генерирует запись, исходящую от нашего абонента абоненту другого оператора
+def generate_cdr_from_our_to_our(subscribers_list, subscribers_other_list):
+    # тип вызова: исходящий или входящий
+    call_type = random.choice([outgoing_call, ingoing_call])
+
+    # номер абонента должен выбираться из БД запросом SELECT,
+    # а дальше - рандомный выбор в коде
+
+    # обслуживаемый абонент выбирается из БД
+    our_subscriber = random.choice(subscribers_list)
+    # второй также выбирается из БД
+    other_subscriber = random.choice(subscribers_other_list)
+
+    # генерируется дата и время начала звонка
+    date_part = fake.date_between(start_date='-1y', end_date='-1d')
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+    
+    start_datetime = datetime.datetime(date_part.year, date_part.month, date_part.day, hour, minute, second)
+
+    # продолжительность звонка от 1 секунды до получаса
+    duration = random.randint(1, 1800)
+
+    # дата и время окончания звонка
+    end_datetime = start_datetime + datetime.timedelta(seconds=duration)
+
+    # генерируем запись
+    entries = []
+    entries.append([call_type, our_subscriber, other_subscriber,
+                    start_datetime.isoformat(), end_datetime.isoformat()])
+            
+    call_type_anti = ingoing_call if call_type == outgoing_call else outgoing_call
+
+    entries.append([call_type_anti, other_subscriber, our_subscriber,
+                    start_datetime.isoformat(), end_datetime.isoformat()])
+    return entries
+
+# генерирует запись, исходящую от нашего абонента абоненту другого оператора
+def generate_cdr_from_our_to_our_midnight(subscribers_list, subscribers_other_list):
+    # тип вызова: исходящий или входящий
+    call_type = random.choice([outgoing_call, ingoing_call])
+
+    # номер абонента должен выбираться из БД запросом SELECT,
+    # а дальше - рандомный выбор в коде
+
+    # обслуживаемый абонент выбирается из БД
+    our_subscriber = random.choice(subscribers_list)
+    # второй также выбирается из БД
+    other_subscriber = random.choice(subscribers_other_list)
+
+    # генерируется дата и время начала звонка
+    date_part = fake.date_between(start_date='-1y', end_date='-1d')
+    hour = 23
+    minute = random.randint(50, 59)
+    second = random.randint(0, 59)
+    
+    start_datetime = datetime.datetime(date_part.year, date_part.month, date_part.day, hour, minute, second)
+
+    # продолжительность звонка от 10 минут и 1 секунды до получаса
+    duration = random.randint(601, 1800)
+
+    # дата и время окончания звонка
+    end_datetime = start_datetime + datetime.timedelta(seconds=duration)
 
     # генерируем записи
     entries = []
-    # если звонок пересёк границу полуночи,
-    # то его нужно разделить
-    if midnight_call:
-        midnight = datetime.datetime(start_datetime.year, start_datetime.month, start_datetime.day, 23, 59, 59)
-        after_midnight = datetime.datetime(end_datetime.year, end_datetime.month, end_datetime.day, 0, 0, 0)
-        # если звонок осуществлялся между двумя абонентами "Ромашки"
-        # нужно сгенерировать четыре звонковых записи
-        # одна - исходящий вызов абонента А до 23:59:59
-        # вторая - входящий вызов абоненту Б до 23:59:59
-        # третья - исходящий вызов абонента А после 00:00:00
-        # четвёртая - входящий вызов абоненту Б после 00:00:00
-        if double_call:
-            call_type_anti = ingoing_call if call_type == outgoing_call else outgoing_call
-            entries.append([call_type, our_subscriber, other_subscriber,
-                            start_datetime.isoformat(), midnight.isoformat()])
-            entries.append([call_type_anti,other_subscriber, our_subscriber,
-                            start_datetime.isoformat(), midnight.isoformat()])
-            
-            entries.append([call_type, our_subscriber, other_subscriber,
-                            after_midnight.isoformat(), end_datetime.isoformat()])
-            entries.append([call_type_anti,other_subscriber, our_subscriber,
-                            after_midnight.isoformat(), end_datetime.isoformat()])
-        # если звонок осуществлялся между абонентом Ромашки и
-        # абонентов другого оператора, то нужно просто разбить
-        # этот звонок на до полнуночи и после неё
-        else:
-            entries.append([call_type, our_subscriber, other_subscriber,
-                            start_datetime.isoformat(), midnight.isoformat()])
-            entries.append([call_type, our_subscriber, other_subscriber,
-                            after_midnight.isoformat(), end_datetime.isoformat()])
-            
-    else:
-        # если звонок осуществлялся между двумя абонентами "Ромашки"
-        # нужно сгенерировать две звонковых записи
-        # одна - исходящий вызов абонента А
-        # вторая - входящий вызов абоненту Б
-        if double_call:
-            entries.append([call_type, our_subscriber, other_subscriber,
-                            start_datetime.isoformat(), end_datetime.isoformat()])
-            
-            call_type_anti = ingoing_call if call_type == outgoing_call else outgoing_call
-
-            entries.append([call_type_anti, other_subscriber, our_subscriber,
-                            start_datetime.isoformat(), end_datetime.isoformat()])
-        else:
-            entries.append([call_type, our_subscriber, other_subscriber,
-                            start_datetime.isoformat(), end_datetime.isoformat()])
-            
-    return entries, midnight_call
+    call_type_anti = ingoing_call if call_type == outgoing_call else outgoing_call
+    midnight = datetime.datetime(start_datetime.year, start_datetime.month, start_datetime.day, 23, 59, 59)
+    after_midnight = datetime.datetime(end_datetime.year, end_datetime.month, end_datetime.day, 0, 0, 0)
+    entries.append([call_type, our_subscriber, other_subscriber,
+                    start_datetime.isoformat(), midnight.isoformat()])
+    entries.append([call_type_anti,other_subscriber, our_subscriber,
+                    start_datetime.isoformat(), midnight.isoformat()])
+    
+    entries.append([call_type, our_subscriber, other_subscriber,
+                    after_midnight.isoformat(), end_datetime.isoformat()])
+    entries.append([call_type_anti,other_subscriber, our_subscriber,
+                    after_midnight.isoformat(), end_datetime.isoformat()])
+    return entries
 
 def generate_all_entries(num_cdr, num_records = 10):
     # отсортированные записи о звонках для всех n файлов
     all_entries = []
     while len(all_entries) < num_cdr * num_records:
-        entries, is_call_midnight = generate_cdr_entry()
-        # проверить, что свежесгенированный звонок(-ки) не перескается
-        # с уже существующими
-        calls_intersect = False
-        # выбираем из свежих записей абонентов
-        subscribers_called = [entries[0][1], entries[0][2]]
-        for i in range(len(all_entries)):
-            # если один из абонентов раннее был записан в файле с звонками
-            if all_entries[i][1] in subscribers_called or all_entries[i][2] in subscribers_called:
-                # если звонок пересекает границу полуночи
-                # необходимо брать границы двух звонков -
-                # до полуночи и после неё
-                if is_call_midnight:
-                    # определим дату и время начала и
-                    # дату и время окончания звонка
-                    start_datetime = entries[0][3]
-                    # если не было двойного звонка между абонентами Ромашки
-                    # то звонок после полуночи находится на 2 месте
-                    if len(entries) == 2:
-                        end_datetime = entries[1][4]
-                    # если двойной звонон между абонентами Ромашки был
-                    # то звонок после полуночи находится на 3 месте
-                    else:
-                        end_datetime = entries[2][4]
-                # если звонок не пересекает границу полуночи,
-                # то данные о его начале и конце берутся из первой записи
-                else:
-                    start_datetime = entries[0][3]
-                    end_datetime = entries[0][4]
-                # если звонки не перескаются
-                if end_datetime < all_entries[i][3] or start_datetime > all_entries[i][4]:
-                    continue
-                # если пересекаются, то проверка прекращается,
-                # а сгенерированный звонок не добавляется
-                else:
-                    calls_intersect = True
-                    break
-        # если звонок не пересекается с уже существующими
-        if not calls_intersect:
-            for entry in entries:
-                all_entries.append(entry)
+        entries = []
+        # добавим одиночный звонок, совершаемый между Помесячным и другим оператором
+        gen_cdrs = generate_cdr_from_our_to_other(subscribers_month_list)
+        calls_intersect = is_calls_intersect(all_entries, gen_cdrs, is_call_midnight=False)
+        # если сгенерированный звонок не перескается с уже существующими, то добавляем его
+        if calls_intersect == False:
+            for cdr in gen_cdrs:
+                all_entries.append(cdr)
+        # добавим одиночный звонок, совершаемый между Классикой и другим оператором
+        gen_cdrs = generate_cdr_from_our_to_other(subscribers_classic_list)
+        calls_intersect = is_calls_intersect(all_entries, gen_cdrs, is_call_midnight=False)
+        if calls_intersect == False:
+            for cdr in gen_cdrs:
+                all_entries.append(cdr)
+        # добавим зеркальный звонок с классики на помесячный (2 записи)
+        gen_cdrs = generate_cdr_from_our_to_our(subscribers_classic_list, subscribers_month_list)
+        calls_intersect = is_calls_intersect(all_entries, gen_cdrs, is_call_midnight=False)
+        if calls_intersect == False:
+            for cdr in gen_cdrs:
+                all_entries.append(cdr)
+        # добавим зеркальный звонок с помесячного на классику (2 записи)
+        gen_cdrs = generate_cdr_from_our_to_our(subscribers_month_list, subscribers_classic_list)
+        calls_intersect = is_calls_intersect(all_entries, gen_cdrs, is_call_midnight=False)
+        if calls_intersect == False:
+            for cdr in gen_cdrs:
+                all_entries.append(cdr)
+        # добавим зеркальный звонок, пересекающий границу полуночи (4 записи)
+        gen_cdrs = generate_cdr_from_our_to_our_midnight(subscribers_list, subscribers_list)
+        calls_intersect = is_calls_intersect(all_entries, gen_cdrs, is_call_midnight=True)
+        if calls_intersect == False:
+            for cdr in gen_cdrs:
+                all_entries.append(cdr)
     # отсортируем звонки по дате  ивремени их начала
     sorted_entries = sorted(all_entries, key=lambda x: datetime.datetime.strptime(x[3], '%Y-%m-%dT%H:%M:%S'))
     return sorted_entries
@@ -158,9 +195,9 @@ def generate_cdr_file(num_cdr):
 
 # функция для создания негативных тестов
 # с некорректными датой и временем звонка
-def generate_negative_incorrect_datetime(num_negative_cdr=1):
+def generate_negative_incorrect_datetime():
     # отсортированные записи о звонках для всех n файлов
-    all_entries = generate_all_entries(num_negative_cdr)
+    all_entries = generate_all_entries(1)
     # изменяется дата и время начала звонка
     # она должна быть больше даты и времени окончания звонка
     duration = random.randint(1, 1800)
@@ -176,9 +213,9 @@ def generate_negative_incorrect_datetime(num_negative_cdr=1):
 
 # функция для создания негативных тестов
 # ни один из абонентов не принадлежит Ромашке
-def generate_negative_not_romashka(num_negative_cdr=1):
+def generate_negative_not_romashka():
     # отсортированные записи о звонках для всех n файлов
-    all_entries = generate_all_entries(num_negative_cdr)
+    all_entries = generate_all_entries(1)
 
     # переменные для генерации неправильных абонентов
     # временно инициализируем их номерами абонентов Ромашки
@@ -206,9 +243,9 @@ def generate_negative_not_romashka(num_negative_cdr=1):
 
 # функция для создания негативных тестов
 # абонент разговаривает сам с собой
-def generate_negative_call_myself(num_negative_cdr=1):
+def generate_negative_call_myself():
     # отсортированные записи о звонках для всех n файлов
-    all_entries = generate_all_entries(num_negative_cdr)
+    all_entries = generate_all_entries(1)
 
     all_entries[0][2] = all_entries[0][1]
     # данный файл сохраняется
@@ -222,9 +259,9 @@ def generate_negative_call_myself(num_negative_cdr=1):
 
 # функция для создания негативных тестов
 # записи отсортированы по убыванию даты и времени начала звонка
-def generate_negative_incorrect_chronology(num_negative_cdr=1):
+def generate_negative_incorrect_chronology():
     # отсортированные записи о звонках для всех n файлов
-    all_entries = generate_all_entries(num_negative_cdr)
+    all_entries = generate_all_entries(1)
 
     all_entries = reversed(all_entries)
     # данный файл сохраняется
@@ -238,9 +275,9 @@ def generate_negative_incorrect_chronology(num_negative_cdr=1):
 
 # функция для создания негативных тестов
 # записи отсортированы по убыванию даты и времени начала звонка
-def generate_negative_through_midnight(num_negative_cdr=1):
+def generate_negative_through_midnight():
     # отсортированные записи о звонках для всех n файлов
-    all_entries = generate_all_entries(num_negative_cdr)
+    all_entries = generate_all_entries(1)
 
     start_datetime = fake.date_time_between(start_date='-1y', end_date='-1d')
     # продолжительность звонка от 1 секунды до получаса
@@ -261,9 +298,9 @@ def generate_negative_through_midnight(num_negative_cdr=1):
 
 # функция для создания негативных тестов
 # некорректный номер
-def generate_negative_incorrect_number(num_negative_cdr=1):
+def generate_negative_incorrect_number():
     # отсортированные записи о звонках для всех n файлов
-    all_entries = generate_all_entries(num_negative_cdr)
+    all_entries = generate_all_entries(1)
     # генерируем более длинный номер
     all_entries[0][2] = f'7{random.randint(900, 999)}{"".join(random.choices("0123456789", k=10))}'
     # данный файл сохраняется
