949817bbf534a40b38034e7dda868dd0ca15b8d8
Natali969 <81923119+Natali969@users.noreply.github.com>
2025-05-08 20:08:50 +0300

    Add files via upload

--- AutoTests/modulesCDR/work_db.py
+++ AutoTests/modulesCDR/work_db.py
@@ -1,7 +1,7 @@
 import psycopg2
 from psycopg2 import Error
 # Раскладывает CDR-файл на записи, которые записывваются в список
-def get_subscribers_list():
+def get_subscribers_list(request):
     # список номеров абонентов "Ромашки" из БД
     subscribers_list = []
     try:
@@ -15,7 +15,7 @@ def get_subscribers_list():
         )
 
         cursor = connection.cursor()
-        postgreSQL_select_Query = "SELECT msisdn FROM subscribers"
+        postgreSQL_select_Query = "SELECT msisdn FROM subscribers LiMIT 10"
         cursor.execute(postgreSQL_select_Query)
         # Выбор строк из таблицы с абонентами с помощью cursor.fetchall
         subscribers_records = cursor.fetchall()
@@ -32,5 +32,34 @@ def get_subscribers_list():
             print("Соединение с PostgreSQL закрыто")
         return subscribers_list
 
-if __name__ == '__main__':
-    print(get_subscribers_list())
+# Раскладывает CDR-файл на записи, которые записывваются в список
+def get_subscribers_classsic_list():
+    # список номеров абонентов "Ромашки" из БД
+    subscribers_list = []
+    try:
+        # Подключиться к существующей базе данных
+        connection = psycopg2.connect(
+            host="localhost",
+            database="brt-db",  # Подключаемся к базе данных "postgres",  к которой всегда есть доступ
+            user="postgres",
+            password="postgres",
+            port="5432"
+        )
+
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "SELECT msisdn FROM subscribers WHERE tariff_id = 1 LiMIT 10"
+        cursor.execute(postgreSQL_select_Query)
+        # Выбор строк из таблицы с абонентами с помощью cursor.fetchall
+        subscribers_records = cursor.fetchall()
+
+        # добавляем номер каждого из абонентов в список
+        for row in subscribers_records:
+            subscribers_list.append(str(row[0]))
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Соединение с PostgreSQL закрыто")
+        return subscribers_list
